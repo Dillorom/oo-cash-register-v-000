5c435944fb296ffce1fae0443de842e83e00e863
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-10 17:09:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 
 class CashRegister
-  attr_accessor :items, :price, :quantity, :total, :discount, :last_transaction
+  attr_accessor :items, :title, :price, :quantity, :total, :discount, :last_transaction
   @@total = 0
 
 
